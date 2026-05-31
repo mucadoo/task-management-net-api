30a131ef21cb24f9ce32c4989aab161c45b2032f
Samuel Britto <samuelgiordano@live.com>
2026-05-31 15:26:27 -0300

    adiciona DTOs de paginação PagedRequest e PagedResponse genérico

---
+++ TaskManagement.Application/DTOs/PagedRequest.cs
@@ -0,0 +1,9 @@
+namespace TaskManagement.Application.DTOs;
+
+public class PagedRequest
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+
+    public int Skip() => (PageNumber - 1) * PageSize;
+}
