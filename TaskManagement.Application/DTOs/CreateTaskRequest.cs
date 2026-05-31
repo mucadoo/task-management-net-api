084969d55dc72596262e4a42e1dd80c6448b8d31
Samuel Britto <samuelgiordano@live.com>
2026-05-31 16:27:10 -0300

    remove DataAnnotations dos DTOs para evitar conflito com FluentValidation

--- TaskManagement.Application/DTOs/CreateTaskRequest.cs
+++ TaskManagement.Application/DTOs/CreateTaskRequest.cs
@@ -1,15 +1,11 @@
-using System.ComponentModel.DataAnnotations;
 using TaskStatus = TaskManagement.Domain.Enums.TaskStatus;
 
 namespace TaskManagement.Application.DTOs;
 
 public class CreateTaskRequest
 {
-    [Required]
-    [MaxLength(200)]
     public string Title { get; set; } = string.Empty;
 
-    [MaxLength(2000)]
     public string? Description { get; set; }
 
     public DateTime? DueDate { get; set; }
--- TaskManagement.Application/DTOs/UpdateTaskRequest.cs
+++ TaskManagement.Application/DTOs/UpdateTaskRequest.cs
@@ -1,11 +1,9 @@
-using System.ComponentModel.DataAnnotations;
 using TaskStatus = TaskManagement.Domain.Enums.TaskStatus;
 
 namespace TaskManagement.Application.DTOs;
 
 public class UpdateTaskRequest
 {
-    [MaxLength(200)]
     public string? Title { get; set; }
 
     public string? Description { get; set; }
