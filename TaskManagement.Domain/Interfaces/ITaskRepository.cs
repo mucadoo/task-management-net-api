40704db81b97a827b29a72581f65f8583fc9c29d
Samuel Britto <samuelgiordano@live.com>
2026-05-31 15:01:25 -0300

    adiciona interface ITaskRepository com contrato de persistência

---
+++ TaskManagement.Domain/Interfaces/ITaskRepository.cs
@@ -0,0 +1,13 @@
+using TaskEntity = TaskManagement.Domain.Entities.Task;
+using TaskStatus = TaskManagement.Domain.Enums.TaskStatus;
+
+namespace TaskManagement.Domain.Interfaces;
+
+public interface ITaskRepository
+{
+    Task<IEnumerable<TaskEntity>> GetAllAsync(TaskStatus? status, DateTime? dueDate, CancellationToken cancellationToken = default);
+    Task<TaskEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task AddAsync(TaskEntity task, CancellationToken cancellationToken = default);
+    Task UpdateAsync(TaskEntity task, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+}
